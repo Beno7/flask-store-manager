a0e25729a456ea0208c439c841f4750097ff59a5
John Adriel Benolirao <adriel.beno@gmail.com>
2020-05-19 23:31:45 +0800

    add logs

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 from app import app
 from db import db
 
+print('running app')
+
 db.init_app(app)
+
+print('db initialized')
 
 @app.before_first_request # before any request, SQLALCHEMY will run this function
 def create_tables():
     db.create_all() # Will create all registered dbs (see models)
+
+print('create_tables defined')
